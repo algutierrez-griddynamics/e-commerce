40cd2ed176e5a5333f4041cfcfee7598dbd0a811
Brian Vega <BrianVega01@outlook.es>
2024-08-21 18:03:39 -0600

    OrderService structure

---
+++ src/main/java/org/ecommerce/services/impl/OrderServiceImpl.java
@@ -0,0 +1,8 @@
+package org.ecommerce.services.impl;
+
+import org.ecommerce.models.Order;
+import org.ecommerce.services.OrderService;
+
+public class OrderServiceImpl implements OrderService<Order> {
+
+}
