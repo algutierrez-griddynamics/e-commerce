ae4dcf50fd17fbbb9efad4bae15545a6a8b3d1ba
Brian Vega <BrianVega01@outlook.es>
2024-08-26 12:48:26 -0600

    Adding ENTITY_NOT_FOUND to Error enum

--- src/main/java/org/ecommerce/enums/Error.java
+++ src/main/java/org/ecommerce/enums/Error.java
@@ -1,6 +1,7 @@
 package org.ecommerce.enums;
 
     public enum Error {
+        ENTITY_NOT_FOUND("Entity not found"),
         INVALID_EMAIL("Email address provided is invalid."),
         PASSWORD_MISMATCH("Old and current passwords do not match."),
         PASSWORD_FORMAT("The password provided has a wrong format (at least 1 lower & upper case letter, 1 digit and 1 special symbol).");
