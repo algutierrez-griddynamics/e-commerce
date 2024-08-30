4f46afbd8433843965eb2b4a0354a231516e5221
Brian Vega <BrianVega01@outlook.es>
2024-08-30 16:33:12 -0600

    Mocking Product service

---
+++ src/main/java/org/ecommerce/services/impl/ProductServiceImpl.java
@@ -0,0 +1,34 @@
+package org.ecommerce.services.impl;
+
+import org.ecommerce.models.Product;
+import org.ecommerce.services.ProductService;
+
+import java.util.List;
+
+public class ProductServiceImpl implements ProductService {
+
+    @Override
+    public Product create(Product entity) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Product update(Long id, Product entity) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void delete(Long id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<Product> findAll() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Product findById(Long id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
