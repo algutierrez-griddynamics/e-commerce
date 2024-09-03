0ba9ae351e5f0745cab8142ee4cc028fabe9e9d1
Brian Vega <BrianVega01@outlook.es>
2024-09-02 19:03:20 -0600

    Replacing constructors with lombok annotations

--- src/main/java/org/ecommerce/models/BillingInformation.java
+++ src/main/java/org/ecommerce/models/BillingInformation.java
@@ -2,6 +2,7 @@ package org.ecommerce.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
@@ -10,12 +11,11 @@ import java.util.Date;
 @Getter
 @Setter
 @SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BillingInformation extends AddressInformation {
 
     private Long orderId;
     private Date billingDate;
     private Price amount;
-
-    BillingInformation() {}
-
 }
