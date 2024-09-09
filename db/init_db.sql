94411eabe66bb926cb0ef6f35071b0600a4e324e
Brian Vega <BrianVega01@outlook.es>
2024-09-09 17:35:18 -0600

    Updating init_db.sql

--- db/init_db.sql
+++ db/init_db.sql
@@ -1,5 +1,23 @@
+CREATE TABLE IF NOT EXISTS customers (
+    pk_customer_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20),
+    address TEXT,
+    categories_preferences TEXT[]
+);
+
 CREATE TABLE IF NOT EXISTS prices (
     pk_price_id BIGSERIAL PRIMARY KEY,
+    currency_code CHAR(3) NOT NULL,
+    amount NUMERIC(10, 2) NOT NULL,
+    description TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS shipping_fee (
+    pk_shipping_fee_id BIGSERIAL PRIMARY KEY,
     currency_code CHAR(3) NOT NULL,
     amount NUMERIC(10, 2) NOT NULL,
     description TEXT NOT NULL
@@ -55,10 +73,12 @@ CREATE TABLE IF NOT EXISTS address_information (
 CREATE TABLE IF NOT EXISTS shipping_information (
     pk_shipping_information_id BIGSERIAL PRIMARY KEY,
     fk_address_information_id BIGINT,
-    fk_price_id BIGINT,              
+    fk_shipping_fee_id BIGINT,
     tracking_number VARCHAR(100) NOT NULL,
     date DATE NOT NULL,
-    status VARCHAR(25) NOT NULL
+    status VARCHAR(25) NOT NULL,
+    FOREIGN KEY (fk_address_information_id) REFERENCES address_information(pk_address_information_id) ON DELETE SET NULL,
+    FOREIGN KEY (fk_shipping_fee_id) REFERENCES shipping_fee(pk_shipping_fee_id) ON DELETE SET NULL
 );
 
 CREATE TABLE IF NOT EXISTS billing_information (
@@ -72,26 +92,27 @@ CREATE TABLE IF NOT EXISTS billing_information (
 
 CREATE TABLE IF NOT EXISTS payment_details (
     pk_payment_details_id BIGSERIAL PRIMARY KEY,
-    fk_price_id BIGINT,               
     payment_method_type VARCHAR(25) NOT NULL,
     card_number VARCHAR(25) NOT NULL,
     card_expiration_date DATE NOT NULL,
     card_holder_name VARCHAR(100) NOT NULL,
     card_cvv VARCHAR(3) NOT NULL,
-    date DATE NOT NULL,
-    FOREIGN KEY (fk_price_id) REFERENCES prices(pk_price_id) ON DELETE SET NULL
+    date DATE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS orders (
     pk_order_id BIGSERIAL PRIMARY KEY,
     fk_shipping_information_id BIGINT,
     fk_billing_information_id BIGINT,
-    fk_payment_details_id BIGINT,      
+    fk_payment_details_id BIGINT,
+    fk_customer_id BIGINT,
     date DATE NOT NULL,
     status VARCHAR(25) NOT NULL,
+    total_usd NUMERIC(10, 2) NOT NULL, -- THIS IS THE SUM OF ALL PRODUCTS IN THE ORDER. Should be calculated
     FOREIGN KEY (fk_shipping_information_id) REFERENCES shipping_information(pk_shipping_information_id) ON DELETE SET NULL,
     FOREIGN KEY (fk_billing_information_id) REFERENCES billing_information(pk_billing_information_id) ON DELETE SET NULL,
-    FOREIGN KEY (fk_payment_details_id) REFERENCES payment_details(pk_payment_details_id) ON DELETE SET NULL
+    FOREIGN KEY (fk_payment_details_id) REFERENCES payment_details(pk_payment_details_id) ON DELETE SET NULL,
+    FOREIGN KEY (fk_customer_id) REFERENCES customers(pk_customer_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS products_orders (
