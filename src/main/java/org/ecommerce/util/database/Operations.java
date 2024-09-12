3ec099f0627189566d4e3131c085c83f9582f718
Brian Vega <BrianVega01@outlook.es>
2024-09-11 19:00:08 -0600

    connection creation and usage implementation

---
+++ src/main/java/org/ecommerce/util/database/Operations.java
@@ -0,0 +1,110 @@
+package org.ecommerce.util.database;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import lombok.Getter;
+import org.ecommerce.exceptions.EntityNotFound;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+
+// TODO: Future project improvements based on the tasks requirements is to implement a pool of connections
+@Getter
+public class Operations <T> {
+
+    private final Connection connection;
+
+    public Operations() {
+        Dotenv dotenv = Dotenv.load();
+
+        String url = String.format("%s://%s:%s/%s",
+                dotenv.get("DB_URL"),
+                dotenv.get("HOST"),
+                dotenv.get("DB_PORT"),
+                dotenv.get("DB_NAME"));
+
+        Connection tempConnection = null;
+        try {
+            tempConnection = DriverManager.getConnection(
+                    url,
+                    dotenv.get("DB_USER"),
+                    dotenv.get("DB_PASSWORD")
+            );
+        } catch (SQLException e) {
+            System.err.println("Failed to establish database connection: " + e.getMessage());
+            throw new RuntimeException("Database connection initialization failed", e);
+        } finally {
+            connection = tempConnection;
+        }
+    }
+
+
+    // Run queries that should not have any result, such table creation or a plain insert
+    public void execute(String query, Object... args) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            setParameters(statement, args);
+            statement.executeUpdate();
+        }
+    }
+
+
+    //    Now takes Consumer instead of Object... and exposes internal state (PreparedStatement) to the caller,
+    //    which is not always good, but in this case can give more flexibility to the caller
+    public void execute(String query, Consumer<PreparedStatement> consumer) throws SQLException {
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            consumer.accept(stmt);
+            stmt.executeUpdate();
+        }
+    }
+
+    // Runs a query and returns the result. Handle the case, when the function can return 0 results.
+    // If the number of results is greater than 1, throw an exception
+    T findOne(String query, Function<ResultSet, T> mapper, Object... args) throws SQLException {
+        try (PreparedStatement stmt = connection.prepareStatement(query, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
+            setParameters(stmt, args);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (isOneResult(rs))
+                    return mapper.apply(rs);
+            }
+        }
+        return null;
+    }
+
+    // Runs a query and returns many results (greater than or equal to 0) as a list.
+    List<T> findMany(String query, Function<ResultSet, T> mapper, Object... args) throws SQLException {
+        List<T> results = new ArrayList<>();
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            setParameters(stmt, args);
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    System.out.println(rs.getString(1));
+                    results.add(mapper.apply(rs));
+                }
+            }
+        }
+        return results;
+    }
+
+
+    private void setParameters(PreparedStatement stmt, Object... args) throws SQLException {
+        for (int i = 0; i < args.length; i++) {
+            stmt.setObject(i + 1, args[i]);
+        }
+    }
+
+    private boolean isOneResult(ResultSet rs) throws SQLException {
+        int rowcount = 0;
+        if (rs.last()) {
+            rowcount = rs.getRow();
+            rs.beforeFirst();
+        }
+        if (rowcount == 0)
+            throw new EntityNotFound("No results returned from query: " + rs);
+        else if (rowcount > 1)
+            throw new SQLException("Too many results returned from query: " + rs);
+        return true;
+    }
+}
