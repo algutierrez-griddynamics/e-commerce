862b969f9a9c32d75d0348391ca283af6a69c488
Brian Vega <BrianVega01@outlook.es>
2024-10-09 10:38:29 -0600

    Deleting unnecessary annotation

--- src/test/java/org/ecommerce/util/database/OperationsTest.java
+++ src/test/java/org/ecommerce/util/database/OperationsTest.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -26,7 +25,6 @@ import static org.junit.jupiter.api.Assertions.*;
 //@Import("some.other.config.bean.files")
 class OperationsTest {
     @TestConfiguration
-    @ComponentScan("org.ecommerce")
     static class operationsConfig {
         @Bean
         Operations<Customer> operations(HikariDataSource hikariDataSource) {
