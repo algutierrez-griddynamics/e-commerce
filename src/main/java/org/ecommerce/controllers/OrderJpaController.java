c40255c35a7d7f524e7b8f4d8d8b2ea0f5ce5945
Brian Vega <BrianVega01@outlook.es>
2024-11-05 18:34:39 -0600

    Updating Controllers

--- src/main/java/org/ecommerce/controllers/OrderJpaController.java
+++ src/main/java/org/ecommerce/controllers/OrderJpaController.java
@@ -2,17 +2,20 @@ package org.ecommerce.controllers;
 
 import org.ecommerce.dtos.requests.OrderRequestDTO;
 import org.ecommerce.dtos.responses.OrderDTO;
-import org.ecommerce.models.Response;
+import org.ecommerce.models.services.responses.CreateOrderResponse;
 import org.ecommerce.models.requests.*;
+import org.ecommerce.models.services.responses.GetAllOrdersResponse;
+import org.ecommerce.models.services.responses.GetOrderResponse;
+import org.ecommerce.models.services.responses.UpdateOrderResponse;
 import org.ecommerce.services.jpa.OrderServiceImpl;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
+@Profile("local")
 @RestController
-@RequestMapping("/orders")
 public class OrderJpaController { //implements ControllerOperations<OrderRequestDTO, Long>{ // TODO: Q: Should i refactor this interface to something like ControllerOperations<RequestDTO, ResponseDTO, ID> ?
 
     private final OrderServiceImpl orderService;
@@ -23,28 +26,44 @@ public class OrderJpaController { //implements ControllerOperations<OrderRequest
 
 //    @Override
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<OrderDTO> create(@PathVariable CreateRequest<OrderRequestDTO> request) {
-        OrderDTO response = orderService.create(request);
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    @PostMapping(path = "/orders")
+    public ResponseEntity<CreateOrderResponse> create(@RequestBody CreateRequest<OrderRequestDTO> request) {
+        CreateOrderResponse response = orderService.create(request);
+
+        return new ResponseEntity<CreateOrderResponse>(response, HttpStatus.CREATED);
     }
 
 //    @Override
-    public ResponseEntity<OrderDTO> delete(DeleteRequest<Long> request) {
-        return null;
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping(path = "/orders/{orderId}")
+    public ResponseEntity<OrderDTO> delete(@PathVariable Long orderId) {
+        orderService.delete(orderId);
+        return new ResponseEntity<OrderDTO>(HttpStatus.OK);
     }
 
 //    @Override
-    public ResponseEntity<OrderDTO> update(UpdateRequest<OrderDTO, Long> request) {
-        return null;
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping(path = "/orders")
+    public ResponseEntity<UpdateOrderResponse> update(@RequestBody UpdateRequest<OrderRequestDTO, Long> request) {
+        UpdateOrderResponse response = orderService.update(request);
+
+        return new ResponseEntity<UpdateOrderResponse>(response, HttpStatus.OK);
     }
 
 //    @Override
-    public ResponseEntity<OrderDTO> get(GetRequest<Long> request) {
-        return null;
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(path = "/orders")
+    public GetAllOrdersResponse get() {
+        return orderService.findAll();
     }
 
 //    @Override
-    public ResponseEntity<List<OrderDTO>> getAll(GetAllRequest request) {
-        return null;
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(path = "/orders/{orderId}")
+    public ResponseEntity<GetOrderResponse> get(@PathVariable Long orderId) {
+        GetOrderResponse getOrderResponse = orderService.findById(orderId);
+
+        return new ResponseEntity<GetOrderResponse>(getOrderResponse, HttpStatus.OK);
     }
+
 }
--- src/main/java/org/ecommerce/models/requests/CreateRequest.java
+++ src/main/java/org/ecommerce/models/requests/CreateRequest.java
@@ -2,8 +2,10 @@ package org.ecommerce.models.requests;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class CreateRequest <T> {
     private T data;
