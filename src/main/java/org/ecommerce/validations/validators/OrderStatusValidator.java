96f1beb1badd8c79785b5df56b2fe77483b029d2
Brian Vega <BrianVega01@outlook.es>
2024-11-12 17:21:04 -0600

    Fixing OrderStatus Validation

--- src/main/java/org/ecommerce/validations/validators/OrderStatusValidator.java
+++ src/main/java/org/ecommerce/validations/validators/OrderStatusValidator.java
@@ -2,14 +2,15 @@ package org.ecommerce.validations.validators;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import org.ecommerce.enums.OrderStatus;
 import org.ecommerce.util.Enums;
-import org.ecommerce.validations.constraints.OrderStatus;
+import org.ecommerce.validations.constraints.ValidOrderStatus;
 
 
-public class OrderStatusValidator implements ConstraintValidator<OrderStatus, String> {
+public class OrderStatusValidator implements ConstraintValidator<ValidOrderStatus, OrderStatus> {
 
     @Override
-    public boolean isValid(String value, ConstraintValidatorContext context) {
-        return Enums.isInEnum(value, org.ecommerce.enums.OrderStatus.class);
+    public boolean isValid(OrderStatus value, ConstraintValidatorContext context) {
+        return Enums.isInEnum(value.name(), org.ecommerce.enums.OrderStatus.class);
     }
 }
