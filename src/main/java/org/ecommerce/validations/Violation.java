7661d1dc8a322797e19560cb3b85543f9dcb3180
Brian Vega <BrianVega01@outlook.es>
2024-11-12 14:55:18 -0600

    Violation & ViolationResponse Model

---
+++ src/main/java/org/ecommerce/validations/Violation.java
@@ -0,0 +1,16 @@
+package org.ecommerce.validations;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Getter;
+
+@JsonSerialize
+@Getter
+public class Violation {
+    private String field;
+    private String message;
+
+    public Violation(String field, String message) {
+        this.field = field;
+        this.message = message;
+    }
+}
