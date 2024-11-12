9247e62aa248839b8aa4137a9dd4d1cee995b39c
Brian Vega <BrianVega01@outlook.es>
2024-11-12 14:54:26 -0600

    Error Handler implementation

---
+++ src/main/java/org/ecommerce/validations/handlers/ErrorHandlingControllerAdvice.java
@@ -0,0 +1,51 @@
+package org.ecommerce.validations.handlers;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+
+import org.ecommerce.validations.ValidationErrorResponse;
+import org.ecommerce.validations.Violation;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Objects;
+
+
+@ControllerAdvice
+class ErrorHandlingControllerAdvice {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ValidationErrorResponse onConstraintValidationException(ConstraintViolationException e) {
+        ValidationErrorResponse error = new ValidationErrorResponse();
+        for (ConstraintViolation<?> violation : e.getConstraintViolations()) {
+            error.getViolations()
+                    .add(new Violation(violation.getPropertyPath().toString(), violation.getMessage()));
+        }
+        return error;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ValidationErrorResponse onMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ValidationErrorResponse error = new ValidationErrorResponse();
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            int dot = fieldError.getField().indexOf('.') + 1;
+            int defaultMessageLength = Objects.requireNonNull(fieldError.getField()).length();
+
+            error.getViolations().add(new Violation
+                    (
+                            fieldError.getField().substring(Math.max(dot, 0), defaultMessageLength)
+                            , fieldError.getDefaultMessage()
+                    ));
+        }
+        return error;
+    }
+}
