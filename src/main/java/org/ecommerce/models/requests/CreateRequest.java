4e92f8811e9396b87fba34084b070da48420b883
Brian Vega <BrianVega01@outlook.es>
2024-11-12 14:58:53 -0600

    Validating requests for post operation

--- src/main/java/org/ecommerce/models/requests/CreateRequest.java
+++ src/main/java/org/ecommerce/models/requests/CreateRequest.java
@@ -1,12 +1,17 @@
 package org.ecommerce.models.requests;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class CreateRequest <T> {
+    @Valid
+    @NotNull
     private T data;
 }
