ccb0566afb1e1281fc46beacd28535a9b890ea34
Brian Vega <BrianVega01@outlook.es>
2024-11-14 18:39:13 -0600

    OrderJpaServiceImpl logic

--- src/main/java/org/ecommerce/services/jpa/impl/OrderJpaServiceImpl.java
+++ src/main/java/org/ecommerce/services/jpa/impl/OrderJpaServiceImpl.java
@@ -2,6 +2,7 @@ package org.ecommerce.services.jpa.impl;
 
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
+import jdk.dynalink.Operation;
 import org.ecommerce.dtos.requests.OrderRequestDTO;
 import org.ecommerce.dtos.responses.OrderDTO;
 import org.ecommerce.enums.Error;
@@ -13,32 +14,56 @@ import org.ecommerce.models.requests.CreateRequest;
 import org.ecommerce.models.requests.UpdateRequest;
 import org.ecommerce.models.services.responses.*;
 import org.ecommerce.repositories.jpa.OrderJpaRepository;
+import org.ecommerce.services.OperationsService;
+import org.ecommerce.services.jpa.OrderJpaService;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
-public class OrderJpaServiceImpl implements OrderJpaService<OrderRequestDTO, Long> {
+public class OrderJpaServiceImpl implements OrderJpaService <OrderRequestDTO, Long> {
 
     private final OrderJpaRepository orderRepository;
     private final OrderDTOMapper orderDTOMapper;
     private final BuildOrderFromDTORequest buildOrderFromDTORequest;
     private final EntityManager entityManager;
 
-    public OrderJpaServiceImpl(OrderJpaRepository orderRepository, OrderDTOMapper orderDTOMapper, BuildOrderFromDTORequest buildOrderFromDTORequest,  EntityManager entityManager) {
+    // Micro-services
+    private final OperationsService<Product, Long> productService;
+    private final OperationsService<ShippingInformation, Long> shippingInformationService;
+
+    public OrderJpaServiceImpl(OrderJpaRepository orderRepository, OrderDTOMapper orderDTOMapper, BuildOrderFromDTORequest buildOrderFromDTORequest, EntityManager entityManager, OperationsService<Product, Long> productService, OperationsService<ShippingInformation, Long> shippingInformationService) {
         this.orderRepository = orderRepository;
         this.orderDTOMapper = orderDTOMapper;
         this.buildOrderFromDTORequest = buildOrderFromDTORequest;
         this.entityManager = entityManager;
+        this.productService = productService;
+        this.shippingInformationService = shippingInformationService;
     }
 
     @Override
     @Transactional
     public CreateOrderResponse create(CreateRequest<OrderRequestDTO> entity) {
         OrderRequestDTO order = entity.getData();
+
+        // Check payment process
+
+        // Check stock
+
+        // Check shipping
+
+        // Check billing
+
         Order newOrder = buildOrderFromDTORequest.apply(order);
 
+        BigDecimal shippingFee = getTotalOfShipment(order.fk_shipping_information_id());
+
+        BigDecimal totalFromProducts = getTotalOfProducts(order.productsIds());
+
+        newOrder.setTotalUsd(shippingFee.add(totalFromProducts));
 
         Order savedOrder = orderRepository.save(newOrder);
         entityManager.clear();
@@ -93,4 +118,19 @@ public class OrderJpaServiceImpl implements OrderJpaService<OrderRequestDTO, Lon
                         .orElseThrow(() -> new EntityNotFound(Error.ENTITY_NOT_FOUND.getDescription()))
         );
     }
+
+    // TODO: Should the following methods be performed for a client to distinct microservices?
+
+    private BigDecimal getTotalOfProducts(List<Long> productsIds) {
+        return productsIds.stream()
+                .map(productService::findById)
+                .map(product -> product.getPrice().getAmount())
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    private BigDecimal getTotalOfShipment(Long shipmentId) {
+        return shippingInformationService.findById(shipmentId).getShippingCost().getAmount();
+    }
+
+
 }
