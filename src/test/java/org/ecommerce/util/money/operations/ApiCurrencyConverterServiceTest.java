5fda4be31e79a9e5853188cdf1b36aab88a834aa
Brian Vega <BrianVega01@outlook.es>
2024-11-21 12:39:36 -0600

    Disabling ApiCurrencyConverter related tests

--- src/test/java/org/ecommerce/util/money/operations/ApiCurrencyConverterServiceTest.java
+++ src/test/java/org/ecommerce/util/money/operations/ApiCurrencyConverterServiceTest.java
@@ -1,6 +1,7 @@
 package org.ecommerce.util.money.operations;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +16,8 @@ class ApiCurrencyConverterServiceTest {
     @Autowired
     private ApiCurrencyConverterService service;
 
+    @Disabled("Since the number of API calls is settled as 100 per month," +
+            "this test has been disabled to use the 100 calls in specific scenarios")
     @Test
     void fetchData() throws JsonProcessingException {
        Map<String, Double> response = service.fetchCurrenciesMapFromApi();
