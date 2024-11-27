b32dcf804ab7bc3281031d4ae19c7cc332f7f788
Brian Vega <BrianVega01@outlook.es>
2024-11-26 18:33:43 -0600

    Updating Location model

--- src/main/java/org/ecommerce/models/Location.java
+++ src/main/java/org/ecommerce/models/Location.java
@@ -5,12 +5,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Setter
 @Getter
 @Entity
+@SuperBuilder
 @Table(name = "locations")
+@NoArgsConstructor
 @AttributeOverride(name="id", column=@Column(name="pk_location_id"))
 public class Location extends AddressInformation {
     int width_cm;
