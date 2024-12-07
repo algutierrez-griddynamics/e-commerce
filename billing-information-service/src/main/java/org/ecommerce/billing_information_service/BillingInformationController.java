38fc2dd1ad6293a2b684714fa81cf760cb3b735c
Brian Vega <BrianVega01@outlook.es>
2024-12-06 18:07:55 -0600

    Circuit breaker, Rate limiter and Bulkhead for billing information service

--- billing-information-service/src/main/java/org/ecommerce/billing_information_service/BillingInformationController.java
+++ billing-information-service/src/main/java/org/ecommerce/billing_information_service/BillingInformationController.java
@@ -1,5 +1,8 @@
 package org.ecommerce.billing_information_service;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.ecommerce.billing_information_service.models.BillingInformation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -16,16 +19,35 @@ public class BillingInformationController {
     private Environment environment;
 
     @GetMapping(path = "/validate/{billingId}")
+    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+    @RateLimiter(name = "default")
+    @Bulkhead(name = "default")
     public boolean validateBillingInformation(@PathVariable @NonNull String billingId) {
         return true;
     }
 
     @GetMapping(path = "/get/{billingId}")
+    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+    @RateLimiter(name = "default")
+    @Bulkhead(name = "default")
     public BillingInformation getBillingInformation(@PathVariable @NonNull Long billingId) {
         System.out.println("Retrieving BillingInformation from port: " + environment.getProperty("local.server.port", "unknown")
         + " at " + LocalDateTime.now() + " ");
         return new BillingInformation(billingId);
     }
 
+    @GetMapping(path = "test-resilience")
+    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+    @RateLimiter(name = "default")
+    @Bulkhead(name = "default")
+    public String unavailableMethod() {
+        System.out.println("Hit unavailable method");
+        throw new RuntimeException("Method called");
+    }
+
+    public String fallbackMethod(Throwable throwable) {
+        System.out.println("Hit fallback method");
+        return "Service unavailable";
+    }
 
 }
