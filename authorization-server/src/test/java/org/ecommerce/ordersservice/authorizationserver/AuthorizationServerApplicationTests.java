c4f3ea803c3bfe2e4377af62f1f7ea6ad7ac07e6
Brian Vega <BrianVega01@outlook.es>
2024-12-17 10:22:26 -0600

    First commit authorization server

---
+++ authorization-server/src/test/java/org/ecommerce/ordersservice/authorizationserver/AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ecommerce.ordersservice.authorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
