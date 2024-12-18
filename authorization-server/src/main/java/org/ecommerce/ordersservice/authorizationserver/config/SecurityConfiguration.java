96fcf423147bf942589a66843d2584a59c4019dc
Brian Vega <BrianVega01@outlook.es>
2024-12-18 10:17:54 -0600

    SecurityFilterChain bean configuration for intercept requests and authenticate them

--- authorization-server/src/main/java/org/ecommerce/ordersservice/authorizationserver/config/SecurityConfiguration.java
+++ authorization-server/src/main/java/org/ecommerce/ordersservice/authorizationserver/config/SecurityConfiguration.java
@@ -6,6 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -16,7 +22,10 @@ public class SecurityConfiguration {
     @Bean
     @Order(SecurityProperties.BASIC_AUTH_ORDER)
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests((request) -> request.anyRequest().authenticated());
+        httpSecurity.authorizeHttpRequests(
+                (request) -> request.requestMatchers("/test").authenticated()
+                        .requestMatchers("/login", "/error").permitAll()
+        );
         httpSecurity.formLogin(withDefaults());
         httpSecurity.httpBasic(withDefaults());
 
